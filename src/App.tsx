d10ca8cbbf491fb7099f2c41673375fd9dbb43ea
CarlosEduardoVdeOliveira <vieiradeoliveira70@gmail.com>
2025-08-25 20:17:31 -0300

    project initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import "./App.css";
-
 function App() {
   return (
     <>
-      <h1 className="color: blue;">Hello world!</h1>
+      <h1 className="text-2xl text-blue-700">Hello world!</h1>
     </>
   );
 }
